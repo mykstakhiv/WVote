3b937eb6f473545a6555d735af76ef202ea4f0f4
mykstakhiv <mykhailostakhiv2004@gmail.com>
2024-09-20 12:50:16 +0100

    Fix nested if statement

--- Wvote/RegisterForm.cs
+++ Wvote/RegisterForm.cs
@@ -21,64 +21,57 @@ namespace Wvote
 
         private void Register(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(FullNameText.Text) &&
-                !string.IsNullOrEmpty(EmailText.Text) &&
-                !string.IsNullOrEmpty(passwordT.Text))
+            if (string.IsNullOrEmpty(FullNameText.Text) &&
+                string.IsNullOrEmpty(EmailText.Text) &&
+                string.IsNullOrEmpty(passwordT.Text))
             {
 
-                bool check = CheckVoter(EmailText.Text);
-
-                if (check == false)
-                {
-
-                    string connectionString = conn.ConnectionString;
-
-                    Regex symbols = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$");
-
-                    if (EmailText.Text.Contains("@"))
-                    {
-                        if (symbols.IsMatch(passwordT.Text)) {
-                            using (SqlConnection con = new SqlConnection(connectionString))
-                            {
-                                string sqlQuery = "INSERT INTO Voter (FullName, Email, Password) VALUES (@FullName, @Email, @Password)";
-
-                                using (SqlCommand sc = new SqlCommand(sqlQuery, con))
-                                {
-                                    sc.Parameters.AddWithValue("@FullName", FullNameText.Text);
-                                    sc.Parameters.AddWithValue("@Email", EmailText.Text);
-
-                                    string hashedPassword = HashPassword(passwordT.Text);
-                                    sc.Parameters.AddWithValue("@Password", hashedPassword);
-
-                                    con.Open();
-                                    sc.ExecuteNonQuery();
-                                    con.Close();
-                                }
-                            }
-
-                            FullNameText.Text = null;
-                            EmailText.Text = null;
-                            passwordT.Text = null;
-                        }
-                        else
-                        {
-                            MessageBox.Show("Make sure that your password is secure");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Please enter correct email address");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("User already exists");
-                }
-            }
-            else
-            {
                 MessageBox.Show("Please fill all boxes");
+                return;
             }
+
+            bool check = CheckVoter(EmailText.Text);
+
+            if (!check == false)
+            {
+                MessageBox.Show("User already exists");
+                return;
+            }
+            if (!EmailText.Text.Contains("@"))
+            {
+                MessageBox.Show("Please enter correct email address");
+                return;
+            }
+
+            Regex symbols = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$");
+            if (!symbols.IsMatch(passwordT.Text))
+            {
+                MessageBox.Show("Make sure that your password is secure");
+                return;
+            }
+
+            string connectionString = conn.ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "INSERT INTO Voter (FullName, Email, Password) VALUES (@FullName, @Email, @Password)";
+
+                using (SqlCommand sc = new SqlCommand(sqlQuery, con))
+                {
+                    sc.Parameters.AddWithValue("@FullName", FullNameText.Text);
+                    sc.Parameters.AddWithValue("@Email", EmailText.Text);
+
+                    string hashedPassword = HashPassword(passwordT.Text);
+                    sc.Parameters.AddWithValue("@Password", hashedPassword);
+
+                    con.Open();
+                    sc.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            FullNameText.Text = null;
+            EmailText.Text = null;
+            passwordT.Text = null;
         }
 
         public bool CheckVoter(string email)
